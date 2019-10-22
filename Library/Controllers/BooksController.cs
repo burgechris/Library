03fb1b0691c410e0b0eff880b956143603d879f1
Student <example@example.com>
2019-10-22 16:42:26 -0700

    add authentication.

--- Library/Controllers/BooksController.cs
+++ Library/Controllers/BooksController.cs
@@ -4,20 +4,28 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace Library.Controllers
 {
+    [Authorize]
     public class BooksController : Controller
-    {
+    {    
         private readonly LibraryContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public BooksController(LibraryContext db)
+        public BooksController(UserManager<ApplicationUser> userManager, LibraryContext db)
         {
+            _userManager = userManager;
             _db = db;
         }
 
         public ActionResult Index()
         {
+            
             return View(_db.Books.ToList());
         }
         public ActionResult Details(int id)
